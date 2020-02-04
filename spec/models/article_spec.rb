8110d026573050bcec48187edacb2ec1c66c0158
anami-hint <anami@hint.com>
2020-02-03 17:49:41 -0800

    Create tests for validating a new article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    it "Should test if the factory is valid" do
+      expect(build :article).to be_valid
+    end
+
+    it "Should validate the presence of the title" do
+      article = build :article, title: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:title]).to include("can't be blank")
+    end
+
+    it "Should validate the presence of the content" do
+      article = build :article, content: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:content]).to include("can't be blank")
+    end
+
+    it "Should validate the presence of the slug" do
+      article = build :article, slug: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:slug]).to include("can't be blank")
+    end
+
+    it "Should validate the uniqueness of the slug" do
+      article = create :article
+      invalid_article = build :article, slug: article.slug
+      expect(invalid_article).not_to be_valid
+    end
+  end
 end
