cb186dcd3c4b07ce52c2c69f6d5c1ab28f1566f6
anami-hint <anami@hint.com>
2020-02-06 10:23:55 -0800

    Replace json methods with helper methods

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
-
   describe "GET #index" do
+    subject { get :index }
     it "returns http success" do
-      get :index
+      subject
       expect(response).to have_http_status(:success)
     end
 
     it "returns proper json" do
       articles = create_list :article, 2
-      #byebug
-      get :index
-      json = JSON.parse(response.body)
-      json_data = json['data']
+      subject
       articles.each_with_index do |article, index|
         expect(json_data[index]['attributes']).to eq({
           "title" => article.title,
